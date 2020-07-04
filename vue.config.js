104599bdc86bb53977b33fb26a3199bba171a581
myst <mystster@gmail.com>
2020-07-04 21:13:53 +0000

    enable electron:build --win

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,22 @@ module.exports = {
   transpileDependencies: ['vuetify'],
   pluginOptions: {
     electronBuilder: {
-      nodeIntegration: false
+      nodeIntegration: true,
+      builderOptions: {
+        appId: 'mystster.redplanner',
+        nsis: {
+          oneClick: false,
+          allowToChangeInstallationDirectory: false
+        },
+        copyright: 'Copyright 2020 mystster',
+        productName: 'RedPlanner',
+        generateUpdatesFilesForAllChannels: true,
+        publish: {
+          releaseType: 'release',
+          provider: 'github',
+          channel: 'alpha'
+        }
+      }
     }
   }
 };
